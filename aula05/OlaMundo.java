181251a69c68a2fd990941e3af5408683a20b686
ccattani <cheycattani@gmail.com>
2026-05-18 22:31:08 -0300

    Initial commit

---
+++ aula05/OlaMundo.java
@@ -0,0 +1,5 @@
+class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!"); // saída do nosso programa
+    }
+}
---
+++ aula06/Argumentos2.java
@@ -0,0 +1,5 @@
+class Argumentos2 {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo! " + args[0]);
+    }    
+}
